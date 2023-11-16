c058dd6ddef1355eb87574358e14d5435c1fe099
김성범 <goldencorn7@naver.com>
2023-11-16 16:47:07 +0900

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, S2D service Demo ! - v4.0")
+	fmt.Fprintf(w, "ICT 기획 운영부 인턴 과제 테스트용 입니다 ! - v4.0")
 }
 
 func main() {
