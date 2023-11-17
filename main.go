8d33f1b162b5b06cf8eaf3f8eaa78ad48e332f60
김성범 <goldencorn7@naver.com>
2023-11-17 11:10:55 +0900

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func index(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "ICT 기획 운영부 인턴 과제 테스트용 입니다 ! - v5.0")
+	fmt.Fprintf(w, "ICT 기획 운영부 인턴 과제 테스트용 입니다 ! - v6.0")
 }
 
 func main() {
